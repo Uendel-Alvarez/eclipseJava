4f93d389efb5dc843bf3a285a323631cdcdcfe1b
Uendel-Alvarez <68794514+Uendel-Alvarez@users.noreply.github.com>
2022-12-16 17:55:46 -0300

    Arquivos adicionados via upload
    Arquivos adicionados por upload para teste de operações com GitHub

---
+++ OlaGitHub/src/br/com/uendel/git/OlaGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.uendel.git;
+
+/**
+ * @author uendel
+ * Teste de integração com git hub
+ */
+public class OlaGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("****aplicação teste com Git****");
+
+	}
+
+}
